5128569bba3c5254615066a2d54b3a1aea4390c5
Will Liu <WillLiuWillLiu@gmail.com>
2025-01-17 18:35:28 -0500

    adds random number

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,39 @@
 'use cache';
 
-export default async function Page() {
-  const randomNumber = Math.random();
+import { headers } from 'next/headers';
+
+async function getSpells() {
   const response = await fetch(
-    'https://www.randomnumberapi.com/api/v1.0/random?min=1&max=100'
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/template_spells?select=*`,
+    {
+      headers: {
+        'apikey': process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        'Authorization': `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
+        'prefer': 'count=exact'
+      },
+    }
   );
-  const [number] = await response.json();
-  return <main className="text-4xl text-center p-12">Random 1: {number}, Random 2: {randomNumber}</main>;
+  
+  if (!response.ok) {
+    throw new Error('Failed to fetch spells');
+  }
+  
+  const data = await response.json();
+  console.log("🚀 ~ getSpells ~ data:", data);
+  return data;
+}
+
+export default async function Page() {
+  const spells = await getSpells();
+  const randomNumber = Math.random();
+  return (
+    <main className="p-12">
+      <h1 className="text-4xl mb-6">Spells - Random number: {randomNumber}</h1>
+      {spells.map((spell: any) => (
+        <div key={spell.id}>
+          <h2>{spell.name}</h2>
+        </div>
+      ))}
+    </main>
+  );
 }
