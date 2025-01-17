d2adf9261f5f51c66faa8e8ff177e19b5dd3bee0
Will Liu <WillLiuWillLiu@gmail.com>
2025-01-17 18:25:43 -0500

    adds random number

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 'use cache';
 
 export default async function Page() {
+  const randomNumber = Math.random();
   const response = await fetch(
     'https://www.randomnumberapi.com/api/v1.0/random?min=1&max=100'
   );
   const [number] = await response.json();
-  return <main className="text-4xl text-center p-12">{number}</main>;
+  return <main className="text-4xl text-center p-12">Random 1: {number}, Random 2: {randomNumber}</main>;
 }
